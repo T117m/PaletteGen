0b87205db4695acf7cf1490d986c7a8ae12fbb9f
T117m <timofey.goryachev@proton.me>
2026-07-01 22:31:08 +0300

    QoL refactoring

--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module github.com/T117m/PaletteGen
 
 go 1.26.3
+
+require golang.org/x/term v0.44.0
+
+require golang.org/x/sys v0.46.0 // indirect
--- internal/utils/cli.go
+++ internal/utils/cli.go
@@ -1,20 +1,18 @@
 package utils
 
 import (
+	"golang.org/x/term"
+
 	"fmt"
 	"image/color"
 	"log"
+	"os"
 )
 
 func PrintPalette(p []color.Color) {
 	for i, c := range p {
 		if c != nil {
-			var (
-				r, g, b, _ = c.RGBA()
-				s          = fmt.Sprintf("\x1b[38;2;%d;%d;%dm██\x1b[0m", uint8(r), uint8(g), uint8(b))
-			)
-
-			fmt.Printf("%d: %s %+v\n", i, s, c)
+			fmt.Printf("%d: %s %+v\n", i, drawPixel(c), c)
 		}
 	}
 }
@@ -22,25 +20,36 @@ func PrintPalette(p []color.Color) {
 func DisplaImage(filePath string) {
 	img, err := LoadImage(filePath)
 	if err != nil {
-		log.Fatalf("Error displaying image: %s", err)
+		log.Fatalf("Error loading image to display: %s", err)
 	}
 
 	var (
 		bounds        = img.Bounds()
 		width, height = bounds.Dx(), bounds.Dy()
+
+		fd = int(os.Stdout.Fd())
 	)
+
+	tWidth, tHeight, err := term.GetSize(fd)
+	if tWidth < width*2 || tHeight < height {
+		fmt.Printf("[!] Image too big to display in terminal: %dx%d vs %dx%d\n", width, height, tWidth/2, tHeight)
+		return
+	}
+
+	if !term.IsTerminal(fd) {
+		fmt.Println("Not a terminal")
+		return
+	}
 
 	for y := range height {
 		for x := range width {
-			var (
-				pixel      = img.At(x, y)
-				rgba       = color.RGBAModel.Convert(pixel)
-				r, g, b, _ = rgba.RGBA()
-				s          = fmt.Sprintf("\x1b[38;2;%d;%d;%dm██\x1b[0m", uint8(r), uint8(g), uint8(b))
-			)
-			fmt.Print(s)
+			fmt.Print(drawPixel(color.RGBAModel.Convert(img.At(x, y))))
 		}
 		fmt.Println()
 	}
 }
 
+func drawPixel(c color.Color) string {
+	r, g, b, _ := c.RGBA()
+	return fmt.Sprintf("\x1b[38;2;%d;%d;%dm██\x1b[0m", uint8(r), uint8(g), uint8(b))
+}
